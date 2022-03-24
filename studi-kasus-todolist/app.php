d54fad55f68f5ce4810f2887a843e6e35fd610c1
marselinusharson <marselinus@undiksha.ac.id>
2022-03-24 14:20:48 +0800

    membuat function showtodolist

--- studi-kasus-todolist/app.php
+++ studi-kasus-todolist/app.php
@@ -3,5 +3,7 @@ require_once ("model/todolist.php");
 require_once("business-logic/show-todolist.php");
 require_once("business-logic/add_todolist.php");
 require_once("business-logic/remove_todolist.php");
-
+require_once("view/view-show-todolist.php");
+require_once("view/view-add-todolist.php");
+require_once("view/view-remove-todolist.php");
 echo "Aplikasi Todolist".PHP_EOL;
--- studi-kasus-todolist/business-logic/show-todolist.php
+++ studi-kasus-todolist/business-logic/show-todolist.php
@@ -4,5 +4,9 @@ menampilkan todo dalam list
 */
 function showTodoList()
 {
-
+    global $todolist;
+    echo "TODOLIST".PHP_EOL;
+    foreach($todolist as $number =>$value){
+        echo"$number. $value".PHP_EOL;
+    }
 }

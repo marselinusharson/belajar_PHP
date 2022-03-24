8f43024c7d98fdc6e6861f56140859687019f929
marselinusharson <marselinus@undiksha.ac.id>
2022-03-24 10:02:27 +0800

    pass reference

--- dasar/reference.php
+++ dasar/reference.php
@@ -4,6 +4,15 @@ $name = "marselinus";
 $otherName = &$name;
 echo $otherName.PHP_EOL;
 echo $name.PHP_EOL;
+
 $otherName ="Randi";
 echo $name.PHP_EOL;
 echo $otherName.PHP_EOL;
+
+function increment(&$value){
+    $value++;
+}
+
+$counter =1;
+increment($counter);
+echo "Counter = $counter";

cbe228dca3b2ee78c3f8476266befa884b61db16
marselinusharson <marselinus@undiksha.ac.id>
2022-03-24 09:36:10 +0800

    include_once() & require_once()

--- dasar/require_include.php
+++ dasar/require_include.php
@@ -1,5 +1,6 @@
 <?php
-require "lib/my_function.php";
-
+require_once "lib/my_function.php";
 echo sayHallo("Marselinus","Rewo");
+require_once "lib/my_function.php";
+
 echo sayHallo("Randi","Loke");
